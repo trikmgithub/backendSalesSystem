ee5356d7971a587beaaee96149fc7ebb653b9cdf
trikmgithub <trikmforwork@gmail.com>
2025-01-11 16:24:17 +0700

    Basic crud user (#3)

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
-    email: string;
-    password: string;
-    name: string;
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  email: string;
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
+  name: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -7,19 +15,39 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(
-    @Body() createUserDto: CreateUserDto
-  ) {
+  //create user
+  @Post('/create')
+  create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
+  //login
   @Get()
-  login(
-    @Body('email') email: string,
-    @Body('password') password: string
-
-  ) {
+  login(@Body('email') email: string, @Body('password') password: string) {
     return this.usersService.login(email, password);
   }
+
+
+  //find one user by id
+  @Get(':id')
+  findOne(
+    @Param('id')
+    id: string
+  ) {
+    return this.usersService.findOne(id);
+  }
+
+  //update user
+  @Patch()
+  update(@Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(updateUserDto);
+  }
+
+  //remove user by id
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(id);
+  }
+
+  
 }
