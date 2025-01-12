7ff46b835d0742b3462b9a5176adf84b94774d70
trikmgithub <trikmforwork@gmail.com>
2025-01-12 23:15:39 +0700

    CORS, soft delete (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+
 @Module({
   imports: [
     MongooseModule.forRootAsync({
--- src/auth/passport/local.strategy.ts
+++ src/auth/passport/local.strategy.ts
@@ -4,14 +4,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
-        super();
+  constructor(private authService: AuthService) {
+    super();
+  }
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException();
     }
-    async validate(username: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(username, password);
-        if (!user) {
-            throw new UnauthorizedException();
-        }
-        return user;
-    }
+    return user;
+  }
 }
